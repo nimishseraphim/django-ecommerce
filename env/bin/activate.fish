a79e1e3de57a279ec1187e2796ea9e558a02bf5a
Nimish Seraphim <nimishseraphim@gmail.com>
2020-11-27 03:33:11 -0800

    add static directory

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nimishseraphim/carzone-project/env"
+set -gx VIRTUAL_ENV "/Users/nimishseraphim/django-ecommerce/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
